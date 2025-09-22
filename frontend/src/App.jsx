1e0350af73333b1931757faa0a183637dcc46166
Murilo Pina <murilo.mpina@gmail.com>
2025-09-22 18:20:16 -0300

    Adicionando componentes no App.jsx, criando footer e adicionando imagens(Banner e Ícone)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Apresentacao from './components/Apresentacao';
+import Contato from './components/Contato';
+import Depoimentos from './components/Depoimentos';
+import Footer from './components/Footer';
+import Funcionalidades from './components/Funcionalidades';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Hero/>
+      <Apresentacao/>
+      <Contato/>
+      <Depoimentos/>
+      <Funcionalidades/>
+      <Footer/>
+    </div>
   )
 }
 
-export default App
+export default App;
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-[#6E2C00] text-[#F4E1C1] py-4">
+      <div className="text-center text-sm">
+        <h5>
+          &copy; 2025 - Todos os direitos reservados
+        </h5>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
