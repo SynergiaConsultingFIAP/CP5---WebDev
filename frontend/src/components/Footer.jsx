a6f3210547e2df4e1ab229c656a11b3421e5b677
Murilo Pina <murilo.mpina@gmail.com>
2025-09-24 11:00:04 -0300

    Adicionando ícones e email/telefone de contato no footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import XIcon from '../assets/XIcon.svg';
 
 const Footer = () => {
   return (
-    <footer className="fixed z-100 bottom-0 w-full bg-[#145A32] text-[#F4E1C1] py-4">
+    <footer className="z-100 bottom-0 w-full bg-[#145A32] text-[#F4E1C1] py-6 ">
       <div className="text-center text-sm">
-        <h5>
-          &copy; 2025 - Todos os direitos reservados
-        </h5>
+        <div className="flex justify-center space-x-4 mb-2 space-y-4">
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <FaFacebook className="h-6 w-6" />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="h-6 w-6" />
+          </a>
+          <a href="https://x.com" target="_blank" rel="noopener noreferrer">
+            <img src={XIcon} alt="X" className='h-6 w-6' />
+          </a>
+        </div>
+          <div className='flex flex-col items-center space-y-4'>
+          <div className="flex justify-center">
+            <span className=''>Email de Contato - gourmet.on@gmail.com / Telefone: (11) 97832-6521</span>
+          </div>
+          <h5 className="flex justify-center">
+            &copy; 2025 - Todos os direitos reservados
+          </h5>
+          </div>
       </div>
     </footer>
   );
